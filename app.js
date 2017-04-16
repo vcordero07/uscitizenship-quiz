2b3c97f39b63fe5788fde36a3f0f61bba91c430e
vcordero07 <vcordero07@gmail.com>
2017-04-15 23:29:15 -0400

    Wip - adding state setup

---
+++ app.js
@@ -0,0 +1,136 @@
+console.clear();
+//Aye Captain | Indubitably | OK | Sure | Oh Yeah | Yay
+//Uh-uh | Nope | No way, Jose | No Siree | Strike | Negative
+
+let quizAppData = {
+  question1: {
+    question: 'Name one state that borders Mexico.',
+    answers: {
+      a: 'Florida',
+      b: 'Arizona',
+      c: 'Louisiana',
+      d: 'Nevada'
+    },
+    correctAnswer: 'b',
+    userAnswer: null,
+    correctMSG: '',
+    incorrectMSG: ''
+  }
+  question2: {
+    question: 'Name one state that borders Canada.',
+    answers: {
+      a: 'Iowa',
+      b: 'South Dakota',
+      c: 'Montana',
+      d: 'Massachusetts'
+    },
+    correctAnswer: 'c',
+    userAnswer: null,
+    correctMSG: '',
+    incorrectMSG: ''
+  }
+  question3: {
+    question: 'Name one American Indian tribe in the United States.',
+    answers: {
+      a: 'Indians',
+      b: 'RedSkins',
+      c: 'Blackhawks',
+      d: 'Cherokee'
+    },
+    correctAnswer: 'd',
+    userAnswer: null,
+    correctMSG: '',
+    incorrectMSG: ''
+  }
+  //   question4: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question5: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question6: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question7: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question8: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question9: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+  //   question10: {
+  //     question: '',
+  //     answers: {
+  //       a: '',
+  //       b: '',
+  //       c: '',
+  //       d: ''
+  //     },
+  //     correctAnswer: '',
+  //     userAnswer: null,
+  //     correctMSG: '',
+  //     incorrectMSG: ''
+  //   }
+}
